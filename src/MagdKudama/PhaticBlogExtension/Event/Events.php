d583ff9795a11f19e9f30c0ee8bbd1c47b5fc0e4
magdkudama <magdkudama@gmail.com>
2014-01-26 00:27:11 +0100

    Added event before creating homepage

--- src/MagdKudama/PhaticBlogExtension/Event/Events.php
+++ src/MagdKudama/PhaticBlogExtension/Event/Events.php
@@ -6,5 +6,6 @@ final class Events
 {
     const BEFORE_POST_CREATED = 'blog.before.post_created';
     const BEFORE_PAGE_CREATED = 'blog.before.page_created';
+    const BEFORE_HOMEPAGE_CREATED = 'blog.before.homepage_created';
     const AFTER_DUMP_ASSETS = 'blog.before.dump_assets';
 }
--- src/MagdKudama/PhaticBlogExtension/Processor/HomepageProcessor.php
+++ src/MagdKudama/PhaticBlogExtension/Processor/HomepageProcessor.php
@@ -3,6 +3,8 @@
 namespace MagdKudama\PhaticBlogExtension\Processor;
 
 use MagdKudama\Phatic\Processor;
+use MagdKudama\PhaticBlogExtension\Event\BasePageEvent;
+use MagdKudama\PhaticBlogExtension\Event\Events;
 use MagdKudama\PhaticBlogExtension\Model\Page;
 use MagdKudama\PhaticBlogExtension\Parser\BaseParser;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -28,11 +30,14 @@ class HomepageProcessor extends BaseProcessor implements Processor
 
     protected function dumpPage(Page $page)
     {
+        $event = new BasePageEvent($page);
+
         $content = $this->getView()->render(
             '_layouts/homepage.html', []
         );
 
         $page->setPageContent($content);
+        $this->getDispatcher()->dispatch(Events::BEFORE_HOMEPAGE_CREATED, $event);
         $this->getFileSystem()->dumpFile($this->getConfig()->getResultsPath() . 'index.html', $page->getPageContent());
     }
 }
